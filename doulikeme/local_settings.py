f3966a9863eb4bf5b732c6a9b4a6d79743494334
endee416 <nnamdi.aneke.goodness@gmail.com>
2020-09-14 14:39:51 +0100

    demo django pro

--- doulikeme/local_settings.py
+++ doulikeme/local_settings.py
@@ -9,6 +9,6 @@ DATABASES = {
         'USER':'postgres',
         'PASSWORD': '12345',
         'HOST':'localhost',
-        'PORT':'5433'
+        'PORT':'5432'
     }
 }
--- doulikeme/settings.py
+++ doulikeme/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER':'postgres',
         'PASSWORD': '12345',
         'HOST':'localhost',
-        'PORT':'5433'
+        'PORT':'5432'
     }
 }
 
